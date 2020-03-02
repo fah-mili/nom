29f52d81365f0ddac10b8f1bcfc4e5328ded7050
Fanny He <fah@odoo.com>
2020-03-02 14:13:00 +0100

    will it crash

---
+++ test/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "test",
+
+    'summary': """
+        test
+        """,
+    'version': '1.0.0',
+    'author': "fah",
+    'category': 'Referential',
+
+    'depends': [
+        'base'
+    ],
+
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ test/models/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, _
+
+
+class ResCompany(models.Model):
+    """
+        Representation of a company
+    """
+    _inherit = 'res.company'
+
+    code = fields.Char(
+        help='Company unique code',
+        string='Code',
+        required=True
+    )
